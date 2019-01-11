48e0430b8145ded6bd219976ea97618b994b768f
Krukov Dima <glebov.ru@gmail.com>
2019-01-11 08:49:14 +0300

    Oauth2 with google

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
+  Redirect,
 } from 'react-router-dom';
 
-import logo from './logo.svg';
 import { SignIn } from './pages'
 
 const NoMatch = ({ location }) => {
@@ -18,13 +18,42 @@ const NoMatch = ({ location }) => {
   )
 }
 
+const Home = () => {
+  return (
+    <div>HOME SWEET HOME</div>
+  )
+}
+const redirect = () => {
+  let urlParams = new URLSearchParams(window.location.search);
+  
+  const auth = fetch(process.env.REACT_APP_AUTH_URL, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      code: urlParams.get("code"),
+      redirect_uri: process.env.REACT_APP_GOOGLE_REDIRECT_URI
+    }, null, 2),
+    mode: 'cors',
+    cache: 'default'
+  }).then(resp => resp.json()).then(
+    data => window.localStorage.setItem('access_token', data.access_token)
+  )
+  return <Redirect to="/home" />
+}
+
+
+
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
           <Route path="/sign-in" exact component={SignIn} />
-          <Route path="/" exact component={SignIn} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/" exact component={redirect} />
           <Route component={NoMatch} />
         </Switch>
       </Router>
